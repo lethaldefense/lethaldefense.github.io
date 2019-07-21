716fbc018aaab1e65567a5057a3f0374f7dd25a8
lethaldefense <nustadhurnabhai@gmail.com>
2019-07-21 17:02:21 +0530

    responsive front page~!

---
+++ mobi.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("overnav").style.height = "100%";
+  document.getElementById("mbtn").style.display = "none";
+  document.getElementById("xbtn").style.display = "block"
+}
+
+
+function closeNav() {
+  document.getElementById("overnav").style.height = "0%";
+  document.getElementById("xbtn").style.display = "none";
+  document.getElementById("mbtn").style.display = "block";
+}
